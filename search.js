54cd3784af36145a72b06f41d1d6d73df98c02f3
tainguyen0310 <phutai906@gmail.com>
2023-06-15 19:13:08 +0700

    update

--- search.js
+++ search.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
-const MovieName ="Spider-Man: Across the Spider-Verse"
+const keyword ="Spider-Man: Across the Spider-Verse"
 const api_key = "14e906c9d6b22d9c2075880ee5f7423e"
 //search for a movie
-async function searchMovie(movieName) {
+async function searchMovie(keyword) {
     try{
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=${api_key}`)
+        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${keyword}&api_key=${api_key}`)
         const movieData = response.data;
         console.log('Movie info:');
         console.log("name:",movieData.results[0].title);
@@ -18,4 +18,4 @@ async function searchMovie(movieName) {
     }
 }
 
-searchMovie(MovieName);
+searchMovie(keyword);
